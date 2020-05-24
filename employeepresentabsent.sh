d8a7abaab6185a791420b0ded106046aa324f953
riyazthk <er.riyaz2507@gmail.com>
2020-05-24 13:56:27 +0530

    to add employee wages to per hour

--- employeepresentabsent.sh
+++ employeepresentabsent.sh
@@ -2,12 +2,15 @@
 absent=0
 present=1
 attendance=$((RANDOM%2))
-
+rate per hour=20
 case $attendance in
 
 $absent) echo "employee is absent" ;;
 
-$present) echo "employee is present" ;;
+$present) echo "employee present"
+                 fullday=8 ;;
 
 esac
 
+calculate=$(( $fullday * $attendance ))
+echo "$calculate"
