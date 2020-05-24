b86186f5d2645bd87c7f204398ba0b0b2446abd3
riyazthk <er.riyaz2507@gmail.com>
2020-05-24 14:26:26 +0530

    add employee total wages

--- employeepresentabsent.sh
+++ employeepresentabsent.sh
@@ -1,16 +1,26 @@
 #! /bin/bash -x
 absent=0
 present=1
+partitm=0
+fulltime=1
 attendance=$((RANDOM%2))
-rate per hour=20
+rateperhour=20
 case $attendance in
 
 $absent) echo "employee is absent" ;;
 
 $present) echo "employee present"
-                 fullday=8 ;;
+                calculatehr=$((RANDOM%2)) ;;
 
 esac
 
-calculate=$(( $fullday * $attendance ))
-echo "$calculate"
+case $calculatehr in
+
+$fulltime)
+         fulltime=8
+         totalempwages=$(( $rateperhour * $fulltime )) ;;
+$parttime)
+         partime=4
+         totalempwages=$(( $rateperhour * $parttime )) ;;
+esac
+echo $totalempwages
